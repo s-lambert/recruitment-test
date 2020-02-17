62c930bb56effe7636686a32ad1671e071501be8
Scott <s-lambert@live.com.au>
2020-02-17 16:56:22 +0800

    Show a loading message while fetching data

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+
+test('displays a loading message while fetching data', () => {
+  const { getByText } = render(<App />);
+  const loadingMessage = getByText(/loading/i);
+  expect(loadingMessage).toBeInTheDocument();
+});
