43271c566087a6d0e03d7522d7a3131a01758f8c
Scott <s-lambert@live.com.au>
2020-02-17 21:20:41 +0800

    Get a random selection of posts, albums and users

--- src/item-api.ts
+++ src/item-api.ts
@@ -1,3 +1,5 @@
+import { randomPick } from './utils';
+
 interface Post {
   title: string;
 }
@@ -16,6 +18,31 @@ export interface Item {
   user: User;
 }
 
+const fetchJson = (url: string) => fetch(url).then(response => response.json());
+
+const baseUrl = 'https://jsonplaceholder.typicode.com/';
+
+const fetchPosts = () => fetchJson(baseUrl + 'posts') as Promise<Post[]>;
+const fetchAlbums = () => fetchJson(baseUrl + 'albums') as Promise<Album[]>;
+const fetchUsers = () => fetchJson(baseUrl + 'users') as Promise<User[]>;
+
 export async function fetchItems(limit: number): Promise<Item[]> {
-  return Promise.resolve([]);
+  const [posts, albums, users] = await Promise.all([
+    fetchPosts(),
+    fetchAlbums(),
+    fetchUsers()
+  ]);
+  const randomPosts = randomPick(posts, limit);
+  const randomAlbums = randomPick(albums, limit);
+  const randomUsers = randomPick(users, limit);
+
+  const items: Item[] = [];
+  for (let i = 0; i < limit; i++) {
+    items.push({
+      post: randomPosts[i],
+      album: randomAlbums[i],
+      user: randomUsers[i]
+    });
+  }
+  return items;
 }
