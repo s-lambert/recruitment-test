05f9261bf4b8ad9d36e8cf772fe51ee3dc689c59
Scott <s-lambert@live.com.au>
2020-02-18 10:26:26 +0800

    Remove test code that was trying to remove warning

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,8 @@
 jest.mock('./item-api');
-jest.useFakeTimers();
 
 import React from 'react';
 import { render, waitForElement } from '@testing-library/react';
 import App from './App';
-import { act } from 'react-dom/test-utils';
 
 const itemApi = require('./item-api');
 const fetchItemsMock = (itemApi.fetchItems = jest.fn());
@@ -19,9 +17,6 @@ test('displays a loading message while fetching data', () => {
 test('displays a list of items from an API', async () => {
   fetchItemsMock.mockResolvedValueOnce([]);
   const { getByRole } = render(<App />);
-  act(() => {
-    jest.runAllTimers();
-  });
   const list = await waitForElement(() => getByRole('list'));
   expect(list).toBeInTheDocument();
 });
